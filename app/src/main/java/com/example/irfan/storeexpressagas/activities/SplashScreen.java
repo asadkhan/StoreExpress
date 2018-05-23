7158e8dbc59728eb54a845b90f8e3544800581fc
asad <asa_khann@hotmail.com>
2018-05-23 14:41:54 +0500

    4

---
+++ app/src/main/java/com/example/irfan/storeexpressagas/activities/SplashScreen.java
@@ -0,0 +1,56 @@
+package com.example.irfan.storeexpressagas.activities;
+
+import com.example.irfan.storeexpressagas.MainActivity;
+import com.example.irfan.storeexpressagas.R;
+import com.example.irfan.storeexpressagas.baseclasses.BaseActivity;
+import com.example.irfan.storeexpressagas.databinding.ActivitySplashBinding;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends BaseActivity {
+    private static final int SPLASH_TIME_OUT = 3000;
+    ActivitySplashBinding binding;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        binding= DataBindingUtil.setContentView(SplashScreen.this,R.layout.activity_splash);
+        parentBinding=binding;
+        openNewScreen();
+    }
+
+    private void openNewScreen() {
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+            @Override
+            public void run() {
+
+
+                Intent i;
+
+                i = new Intent(SplashScreen.this, MainActivity.class);
+
+
+                //i = new Intent(SplashScreen.this, LanuageSelection.class);
+
+
+                startActivity(i);
+                finish();
+
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+}
