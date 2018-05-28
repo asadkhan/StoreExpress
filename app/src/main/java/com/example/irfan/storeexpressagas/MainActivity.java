a42d4a9913dda33fd88875bdf2340c1a06cd2192
Muhammad Naveed <naveed2309923@gmail.com>
2018-05-28 18:21:04 +0500

    Pro Detail Page Final

--- app/src/main/java/com/example/irfan/storeexpressagas/MainActivity.java
+++ app/src/main/java/com/example/irfan/storeexpressagas/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_shop_by_cate);
+        setContentView(R.layout.activity_pro_detail);
     }
 }
