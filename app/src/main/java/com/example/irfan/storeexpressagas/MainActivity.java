05e2f712b4153a3fe42b4ebdd8e08904eda41264
Muhammad Naveed <naveed2309923@gmail.com>
2018-05-29 18:02:13 +0500

    Checkout Products

--- app/src/main/java/com/example/irfan/storeexpressagas/MainActivity.java
+++ app/src/main/java/com/example/irfan/storeexpressagas/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pro_detail);
+        setContentView(R.layout.activity_cart);
     }
 }
