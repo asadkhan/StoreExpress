22b6392d0b207af590d1f473493e4de287678dd4
Muhammad Naveed <naveed2309923@gmail.com>
2018-05-30 18:15:41 +0500

    2 new Screens

--- app/src/main/java/com/example/irfan/storeexpressagas/MainActivity.java
+++ app/src/main/java/com/example/irfan/storeexpressagas/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cart);
+        setContentView(R.layout.activity_my_orders);
     }
 }
