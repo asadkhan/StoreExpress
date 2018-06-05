1c8c472c00b9471f0b42baaac1d21dfbff2c4168
Muhammad Naveed <naveed2309923@gmail.com>
2018-06-05 18:18:46 +0500

    New Screen

--- app/src/main/java/com/example/irfan/storeexpressagas/MainActivity.java
+++ app/src/main/java/com/example/irfan/storeexpressagas/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_shopping_list);
+        setContentView(R.layout.activity_about);
     }
 }
