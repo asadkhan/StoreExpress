2baf107ef17dfdd10d9846618da6051cc102047d
asad <asa_khann@hotmail.com>
2018-06-20 18:01:27 +0500

    hah

---
+++ app/src/main/java/com/example/irfan/storeexpressagas/network/EndPoints.java
@@ -0,0 +1,8 @@
+package com.example.irfan.storeexpressagas.network;
+
+public class EndPoints {
+
+    static final String API_PREFIX="/api/";
+    static final String CATEGORIES=API_PREFIX+"category/getcategories";
+
+}
