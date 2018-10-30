f652d592e6c1d97f6edc3abacee9ca7d9f87a769
Muhammad Naveed <naveed2309923@hotmail.com>
2018-10-30 08:36:20 -0700

    Online

--- app/src/main/java/com/example/irfan/storeexpressagas/network/NetworkConstants.java
+++ app/src/main/java/com/example/irfan/storeexpressagas/network/NetworkConstants.java
@@ -1,7 +1,7 @@
 package com.example.irfan.storeexpressagas.network;
 
 public class NetworkConstants {
-    public static String BASE_URL="http://192.168.100.6:8080/";
+    public static String BASE_URL="http://www.insidedemo.com/";
     public final static  long CONNECTION_TIMEOUT = 25;
     public final static int DATA_SUCCESS=1;
     public final static int DATA_ERROR=0;
